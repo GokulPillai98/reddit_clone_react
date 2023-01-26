c0ade0cf5a4a84f79c4d7de7894459850ceb617c
Gokul K P <gokul.pillai98@gmail.com>
2023-01-26 22:59:22 +0000

    Theme setup using Chakra and Worked on Header component

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { authModalState } from "@/src/atoms/authModalAtom";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Here is the modal body {modalState.view}</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
